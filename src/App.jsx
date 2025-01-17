dbedb1f1910ddb975bceff0548c0389d4712fb85
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-01-17 22:50:11 +0600

    react headroom used

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import Chart from "./components/Chart/Chart"
 import Navbar from "./components/Navbar/Navbar"
 import PriceOptions from "./components/PriceOptions/PriceOptions"
+import Headroom from 'react-headroom';
+
 
 const App = () => {
   return (
     <div>
-
+      <Headroom>
+        <header style={{ background: '#6200ea', color: 'white', padding: '10px' }}>
+          <h1>Sticky Header</h1>
+        </header>
+      </Headroom>
+      <main style={{ height: '200px', padding: '20px' }}>
+        <p>Scroll down to see the header hide, and scroll up to see it reappear!</p>
+      </main>
       <Navbar></Navbar>
       <PriceOptions></PriceOptions>
       <Chart></Chart>
